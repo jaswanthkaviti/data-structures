cd45ac9dcd72fe307023caebc0501f1d565c9a00
jaswanthkaviti <neikavi3599@gmail.com>
2022-03-13 00:59:50 +0530

    submitting code

---
+++ majorityelement.c
@@ -0,0 +1,34 @@
+#include <stdio.h> 
+int main()
+{
+    int n;
+    printf("array size");
+    scanf("%d",&n);
+    int array[n];
+    int max = 0;
+    int count;
+    int maxelement;
+    for(int i=0; i<n; i++)
+    {
+        printf("enter element");
+        scanf("%d",&array[i]);
+    }
+    for(int i=0; i<n; i++)
+    {
+        count++;
+        for(int j=0; j<n; j++)
+        {
+            if(array[i]==array[j])
+            {
+                count++;
+            }
+        }
+        if(count>max)
+        {
+            max = count;
+            maxelement = array[i];
+        }    
+    }
+    printf("%d",maxelement);
+    return 0;
+}
